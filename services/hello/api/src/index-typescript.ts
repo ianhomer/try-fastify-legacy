7a2c8c4a6b9e7cf98bd22382bcf9efae3d92b32e
Ian Homer <ian@purplepip.com>
2020-10-12 08:52:25 +0100

    Typescript example

---
+++ services/hello/api/src/index-typescript.ts
@@ -0,0 +1,23 @@
+import fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
+import { Server, IncomingMessage, ServerResponse } from 'http';
+
+const server: FastifyInstance<
+  Server,
+  IncomingMessage,
+  ServerResponse
+> = fastify();
+
+// Add our route handler with correct types
+server.get('/', {}, (request, reply) => {
+  console.log(request);
+  reply.code(200).send({ 'Hello': 'World!' });
+});
+
+// Start your server
+server.listen(3000, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(0);
+  }
+  console.log(`Server listening at ${address}`);
+});
