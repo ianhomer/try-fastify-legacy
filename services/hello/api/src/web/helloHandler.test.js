edbc1683ee5f9cd8a281ef80e8892a5f0971ac75
Ian Homer <ian.homer@gmail.com>
2020-10-17 08:54:23 +0100

    Noddy handler test

--- services/hello/api/src/web/helloHandler.test.js
+++ services/hello/api/src/web/helloHandler.test.js
@@ -3,10 +3,10 @@ import helloHandler from "./helloHandler";
 test("hello", () => {
   const mockRequest = jest.fn();
   const mockReply = {
-    code: jest.fn().mockImplementation((code) => {
-      send: jest.fn();
-    }),
+    code: jest.fn().mockReturnThis(),
+    send: jest.fn().mockReturnThis(),
   };
-  //helloHandler(mockRequest, mockReply);
-  //expect(mockReply.mock.calls.length).toBe(1);
+  helloHandler(mockRequest, mockReply);
+  expect(mockReply.code.mock.calls.length).toBe(1);
+  expect(mockReply.send.mock.calls.length).toBe(1);
 });
