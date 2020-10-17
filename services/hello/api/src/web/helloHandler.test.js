03a58e522c693a667aaa69b174a733ff03dcf101
Ian Homer <ian.homer@gmail.com>
2020-10-17 09:00:56 +0100

    Add hello call assertion

--- services/hello/api/src/web/helloHandler.test.js
+++ services/hello/api/src/web/helloHandler.test.js
@@ -8,5 +8,7 @@ test("hello", () => {
   };
   helloHandler(mockRequest, mockReply);
   expect(mockReply.code.mock.calls.length).toBe(1);
+  expect(mockReply.code.mock.calls[0][0]).toBe(200);
   expect(mockReply.send.mock.calls.length).toBe(1);
+  expect(mockReply.send.mock.calls[0][0].message).toBe("Hello, World!");
 });
